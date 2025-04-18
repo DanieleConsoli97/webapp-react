1e466c409c605ab2068ba14ff9a46427da06f0c0
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-04-18 16:49:24 +0200

    fix navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
                 <nav className="flex-no-wrap relative flex w-full items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4">
                     <NavLink to={'/'} className="mb-4 me-5 ms-2 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0 active:bg-blue-600"  >Home</NavLink>
                     <NavLink to={'/Films'} className="mb-4 me-5 ms-2 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"  >Films</NavLink>
-                    <NavLink to={'/film/AddMovie'} className="mb-4 me-5 ms-2 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"  >Add Book</NavLink>
+                    <NavLink to={'/film/AddMovie'} className="mb-4 me-5 ms-2 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"  >Add movie</NavLink>
 
                 </nav>
         </header>
